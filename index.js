6e7607395a1636c91791a486222ef9004ef3e301
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2021-08-29 17:35:39 +1000

    Modify index.js to check docker cache and rebuilds

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there");
+  res.send("by there");
 });
 
 app.listen(8080, () => {
   console.log("Listening on port 8080");
 });
+
