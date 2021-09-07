b766477924d41490fc15879a183be6c4db244dc1
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2021-09-07 20:53:45 +1000

    Modify index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("by there");
+  res.send("Hello, this is Jenny here");
 });
 
 app.listen(8080, () => {
